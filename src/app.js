fc11e9f86afd35905a77805aebc58eda3a529035
AmanYadavFeb22 <ay415282@gmail.com>
2025-08-13 19:52:28 +0530

    Creating Express Server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const app=express()
+const PORT=3000
+
+app.use("/home",(req,res)=>{
+    res.send("Welcome to home section")
+})
+
+app.use("/contact",(req,res)=>{
+   res.send("Welcome To the contact section!")
+   
+})
+
+app.use("/about",(req,res)=>{
+    
+    res.send("Welcome to About Ssection")
+})
+app.listen(PORT)
