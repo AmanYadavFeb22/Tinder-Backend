aea458cd9aa022755d5e2ae6d388641d2bc5ce52
AmanYadavFeb22 <ay415282@gmail.com>
2025-08-14 12:35:59 +0530

    Playing with Path

--- src/app.js
+++ src/app.js
@@ -2,17 +2,30 @@ const express=require("express")
 const app=express()
 const PORT=3000
 
+
+app.use("/home/123",(req,res)=>{
+    res.send("Welcome to home/123 section")
+})
+
 app.use("/home",(req,res)=>{
     res.send("Welcome to home section")
 })
 
 app.use("/contact",(req,res)=>{
-   res.send("Welcome To the contact section!")
-   
+    res.send("Welcome To the contact section!")
+    
 })
+
 
 app.use("/about",(req,res)=>{
     
     res.send("Welcome to About Ssection")
 })
-app.listen(PORT)
+
+app.use("/",(req,res)=>{
+    res.send("Welcome welcome welcome")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
