d97b8f4c85045df5d5e1c4030e875317e4160fb7
AmanYadavFeb22 <ay415282@gmail.com>
2025-08-15 16:44:57 +0530

    reading data from the database

--- src/app.js
+++ src/app.js
@@ -1,41 +1,68 @@
 const express = require("express");
-const connectdb=require('./config/database')
-const {User}=require('./models/user')
+const connectdb = require("./config/database");
+const { User } = require("./models/user");
 const app = express();
 const PORT = 3000;
 
-app.use(express.json())
-app.post("/signup",async(req,res)=>{
-    console.log(req.body)
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  //creating a new instance of User model
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User Details saved successfully");
+  } catch (error) {
+    res.status(500).send("Error saving user data");
+  }
+});
 
-    // const userObj={
-    //     firstName:"Aman",
-    //     lastName:"Yadav",
-    //     emailId:"Aman@gmail.com",
-    //     password:"Aman@123",
-    //     age:12,
-    //     Gender:"Male"
-    // }
-    // creating a new intance of user model
-    const user=new User(req.body)
-    try {
-        await user.save()
-        res.send("User Details saved successfully")
-        
-    } catch (error) {
-        res.status(500).send("Error saving user data")
+//Getting a user data from the database
+app.get("/user", async (req, res) => {
+  const userData = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userData });
+    console.log(user);
+    if (user.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(user);
     }
+  } catch (error) {
+    res.status(400).send("Error sending data");
+  }
+});
+
+// finding only one user
+
+app.get("/user",async(req,res)=>{
+    const userData=req.body.emailId
+    const user=await User.findOne()
+    res.send(user)
 
 })
 
-connectdb().then((res=>{
-    console.log("connected to database successfully")
+app.get("/feed",async(req,res)=>{
+    try {
+        const user=await User.find({})
+        res.send(user)
+        if(user.length===0){
+            res.status(404).send("user not found")
+        }
+
+        
+    } catch (error) {
+        res.status(400).send("Error sending data");
+    }
+})
+
+connectdb()
+  .then((res) => {
+    console.log("connected to database successfully");
     app.listen(PORT, () => {
       console.log(`Server is running at http://localhost:${PORT}`);
     });
-
-})).catch((err)=>{
-    console.log("error connecting to database")
-})
-
-
+  })
+  .catch((err) => {
+    console.log("error connecting to database");
+  });
