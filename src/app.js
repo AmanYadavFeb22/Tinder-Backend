f2cf712484026f0dc94bdf4d62a6463f19100a1a
AmanYadavFeb22 <ay415282@gmail.com>
2025-08-15 18:08:36 +0530

    Performing CRUD operation in database

--- src/app.js
+++ src/app.js
@@ -56,6 +56,33 @@ app.get("/feed",async(req,res)=>{
     }
 })
 
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId
+    try {
+        
+        const user=await User.findByIdAndDelete(userId)
+        res.send("user deleted successfully")
+        
+    } catch (error) {
+        res.status(400).send("Error deleting  data");
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+    const userId=req.body.userId
+    const data=req.body
+    try {
+       
+       const user= await User.findByIdAndUpdate(userId,data)
+       res.send("User updated successfully")
+        
+    } catch (error) {
+         res.status(400).send("Error updating data");
+        
+    }
+
+})
+
 connectdb()
   .then((res) => {
     console.log("connected to database successfully");
